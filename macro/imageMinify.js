451d6b2f8fb9bcb44d1973cb0b57163d91ffeb3a
shohei-ogawa <shohei-ogawa@aquaring.co.jp>
2019-05-14 16:49:20 +0900

    no message

--- macro/imageMinify.js
+++ macro/imageMinify.js
@@ -2,9 +2,9 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 
-imagemin(['src/**/*.{jpg,png}'], 'htdocs', {
+imagemin(['htdocs/**/*.{jpg,png}'], 'htdocs/', {
 	plugins: [
 		imageminMozjpeg({ quality: 80 }),
-		imageminPngquant({ quality: '65-80' })
+		imageminPngquant({ quality: [.65,.8] })
 	]
 }).then(()=>{console.log('Minified image files.');});
