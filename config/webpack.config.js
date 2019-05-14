08a207513a6a51c66bdc883ad35174d9cb7dd155
shohei-ogawa <shohei-ogawa@aquaring.co.jp>
2019-05-14 17:36:49 +0900

    no message

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,44 +1,45 @@
 const path = require('path');
 const glob = require('glob');
 
-const entries = glob.sync('../src/common/js/entries/*.js');
+const mode = process.env.NODE_ENV; // "development" or "production"
+const baseDir = (mode === 'production') ? '../htdocs/' : '../src/';
+const entries = {};
+glob.sync('../src/common/js/entries/*.js').map((file)=>{
+	entries[file] = path.resolve('../src', file);
+});
 
-module.exports = (env, argv) => {
-	const mode = argv.mode; // "development" or "production"
-	const baseDir = (mode === 'production') ? '../htdocs/' : '../src/';
-
-	return {
-		mode: argv.mode || 'production',
-		entry: {
-			index: path.resolve(__dirname, '../src/common/js/entries/'),
-		},
-		output: {
-			path: path.resolve(__dirname, `${baseDir}common/js/`),
-			filename: '[name].bundle.js'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /(node_modules)/,
-					use: [{
-						loader: 'babel-loader',
-						options: {
-							presets: [['@babel/preset-env', {modules: false}]]
-						}
-					}]
-				}
-			]
-		},
-		optimization: {
-			splitChunks: {
-				cacheGroups: {
-					vendor: {
-						test: /node_modules/,
-						name: 'vendor',
-						chunks: 'initial',
-						enforce: true
+module.exports = {
+	mode: mode || 'production',
+	entry: {
+		index: path.resolve(__dirname, '../src/common/js/entries/index.js')
+	},
+	// entry: entries,
+	output: {
+		path: path.resolve(__dirname, `${baseDir}common/js/`),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', {modules: false}]]
 					}
+				}]
+			}
+		]
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /node_modules/,
+					name: 'vendor',
+					chunks: 'initial',
+					enforce: true
 				}
 			}
 		}
