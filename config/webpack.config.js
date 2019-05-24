16db298b9dfd3473767b0f79fba9a69b7093f366
shohei-ogawa <shohei-ogawa@aquaring.co.jp>
2019-05-24 14:09:28 +0900

    開発時にjsのsourcemapを有効に

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,49 +6,50 @@ const mode = process.env.NODE_ENV; // "development" or "production"
 const baseDir = (mode === 'production') ? '../htdocs/' : '../src/';
 const entries = {};
 glob.sync('../src/common/js/entries/*.js').map((file)=>{
-	entries[file] = path.resolve('../src', file);
+  entries[file] = path.resolve('../src', file);
 });
 
 module.exports = {
-	mode: mode || 'production',
-	entry: {
-		index: path.resolve(__dirname, '../src/common/js/entries/index.js')
-	},
-	// entry: entries,
-	output: {
-		path: path.resolve(__dirname, `${baseDir}common/js/`),
-		filename: '[name].bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: [['@babel/preset-env', {modules: false}]]
-					}
-				}]
-			}
-		]
-	},
+  mode: mode || 'production',
+  devtool: (mode === 'development')? 'inline-source-map':false,
+  entry: {
+    index: path.resolve(__dirname, '../src/common/js/entries/index.js')
+  },
+  // entry: entries,
+  output: {
+    path: path.resolve(__dirname, `${baseDir}common/js/`),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {modules: false}]]
+          }
+        }]
+      }
+    ]
+  },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
     })
   ],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /node_modules/,
-					name: 'vendor',
-					chunks: 'initial',
-					enforce: true
-				}
-			}
-		}
-	}
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true
+        }
+      }
+    }
+  }
 };
