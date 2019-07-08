f911b995f920d2b6c762e3823ba25dbe88d25430
shohei-ogawa <shohei-ogawa@aquaring.co.jp>
2019-07-08 10:34:25 +0900

    pug template commit

--- src/common/js/entries/index.js
+++ src/common/js/entries/index.js
@@ -1,13 +1,19 @@
 import initScrollTrigger from '../module/scrollTrigger';
+import objectFitImages from 'object-fit-images';
 
 import {initScrollParallax} from '../module/parallax';
 
+// jQueryを使う場合はこちらを有効にしてください。
+// import $ from 'jquery';
+
+
 document.addEventListener('DOMContentLoaded',() => {
 	initScrollTrigger();
-	initScrollParallax();
+	// initScrollParallax();
 });
-
 
 window.addEventListener('load',() => {
-
 });
+
+// jQueryの処理はこちら
+// $(function(){});
