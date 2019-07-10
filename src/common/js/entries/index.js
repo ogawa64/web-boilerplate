9ae2097bb7e302d36c2454e729ac345fd05a96c6
shohei-ogawa <shohei-ogawa@aquaring.co.jp>
2019-07-10 10:47:10 +0900

    不要な記述削除

--- src/common/js/entries/index.js
+++ src/common/js/entries/index.js
@@ -3,9 +3,6 @@ import objectFitImages from 'object-fit-images';
 
 import {initScrollParallax} from '../module/parallax';
 
-// jQueryを使う場合はこちらを有効にしてください。
-// import $ from 'jquery';
-
 
 document.addEventListener('DOMContentLoaded',() => {
 	initScrollTrigger();
